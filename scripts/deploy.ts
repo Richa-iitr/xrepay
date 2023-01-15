a95c0a2b65dcf1d2df71bd8d405b36b2317f9032
Richa-iitr <richa@cs.iitr.ac.in>
2023-01-15 08:44:24 +0530

    contracts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,14 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = ethers.utils.parseEther("1");
-
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const Lock = await ethers.getContractFactory("XRepay");
+  const lock = await Lock.deploy("0xDE1CC4c8a3026ad79BA6104333bED630B23210cd", "0x15C6b352c1F767Fa2d79625a40Ca4087Fab9a198");
 
   await lock.deployed();
+  
 
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  console.log(`Lock with 1 ETH and unlock timestamp deployed to ${lock.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
